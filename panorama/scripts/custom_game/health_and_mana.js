b918eb954afd7fa495b5c0647a49215a47e314f9
Matthew Broatch <matthew.broatch@intelligentreach.com>
2017-07-15 18:08:34 +0100

    ability point spending

--- panorama/scripts/custom_game/health_and_mana.js
+++ panorama/scripts/custom_game/health_and_mana.js
@@ -46,11 +46,25 @@ $.Every = function(start, time, tick, func){
     })(start,numRan,tickRate,tickCount));
 };
 
-function OnLevelUpClicked()
+function OnLevelUpClicked(stat)
 {
 	var queryUnit = Players.GetLocalPlayerPortraitUnit();
-	GameEvents.SendCustomGameEventToServer( "increase_hero_stat", { "hero" : queryUnit, "stat" : "str" } );
-	UpdateAbilityList();
+    GameEvents.SendCustomGameEventToServer( "increase_hero_stat", { "hero" : queryUnit, "stat" : stat } );
+    CheckAbilityPointsState();
+}
+
+function CheckAbilityPointsState()
+{
+	var queryUnit = Players.GetLocalPlayerPortraitUnit();
+    if (Entities.GetAbilityPoints( queryUnit ) <= 0) {
+        $( "#LevelUpStrButton" ).RemoveClass( "could_level_up" )
+        $( "#LevelUpDexButton" ).RemoveClass( "could_level_up" )
+        $( "#LevelUpIntButton" ).RemoveClass( "could_level_up" )
+    } else {
+        $( "#LevelUpStrButton" ).AddClass( "could_level_up" )
+        $( "#LevelUpDexButton" ).AddClass( "could_level_up" )
+        $( "#LevelUpIntButton" ).AddClass( "could_level_up" )
+    }
 }
 
 function UpdateHealthAndMana()
@@ -62,14 +76,15 @@ function UpdateHealthAndMana()
 	$( "#health-percent" ).style.width = (Entities.GetHealth( queryUnit ) / Entities.GetMaxHealth( queryUnit ) * 100) + "%";
 	$( "#mana-percent" ).style.width = (Entities.GetMana( queryUnit ) / Entities.GetMaxMana( queryUnit ) * 100) + "%";
 	$( "#health-regen" ).text = "+" + Math.round(Entities.GetHealthThinkRegen( queryUnit ) * 10) / 10;
-  $( "#mana-regen" ).text = "+" + Math.round(Entities.GetManaThinkRegen( queryUnit ) * 10) / 10;
+    $( "#mana-regen" ).text = "+" + Math.round(Entities.GetManaThinkRegen( queryUnit ) * 10) / 10;
 	$( "#health-total" ).text = Entities.GetHealth( queryUnit ) + "/" + Entities.GetMaxHealth( queryUnit );
 	$( "#mana-total" ).text = Entities.GetMana( queryUnit ) + "/" + Entities.GetMaxMana( queryUnit );
 	$( "#damage-label" ).text = (Entities.GetDamageMax( queryUnit ) + Entities.GetDamageMin( queryUnit )) / 2 + Entities.GetDamageBonus( queryUnit );
 	$( "#armor-label" ).text = Math.round((Entities.GetPhysicalArmorValue( queryUnit ) + Entities.GetBonusPhysicalArmor( queryUnit )) * 10) / 10;
 	$( "#movespeed-label" ).text = Entities.GetBaseMoveSpeed( queryUnit );
+	// $( "#strength-label" ).text = Entities.GetStrength( queryUnit );
 	if(Entities.IsHero( queryUnit )) {
-		Entities
+		// $.Msg(Game.GetPlayerInfo( 0 ));
 	}
 }
 
@@ -88,7 +103,9 @@ function UpdateHealthAndMana2()
 	// GameEvents.Subscribe( "dota_player_update_selected_unit", UpdateAbilityList );
 	// GameEvents.Subscribe( "dota_player_update_query_unit", UpdateAbilityList );
 	// GameEvents.Subscribe( "dota_ability_changed", UpdateAbilityList );
-	// GameEvents.Subscribe( "dota_hero_ability_points_changed", UpdateAbilityList );
-	
+	GameEvents.Subscribe( "dota_hero_ability_points_changed", CheckAbilityPointsState );
+    
+    CheckAbilityPointsState();
+
 	$.Every(0, -1, 0.33,  function() { UpdateHealthAndMana() }, 100); // initial update
 })();
